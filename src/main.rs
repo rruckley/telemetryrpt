f32076718c466772186bb6c917a9cc1e0db41024
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-02-22 10:16:42 +1100

    Sample code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::path::PathBuf;
+
+use pdf::error::PdfError;
+use pdf::file::File;
+
+
+fn main() -> Result<(),PdfError> {
+
+    let mut file = File::open("a.pdf").unwrap();
+
+    
+
+    Ok(())
 }
