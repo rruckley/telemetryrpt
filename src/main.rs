9d7bf0da8c0be5e8110d9a15bc5d1fac500d74c8
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-02-23 11:24:12 +1100

    create pdf

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-use std::env;
-use std::path::PathBuf;
+use std::io::BufWriter;
 
 use pdf::error::PdfError;
-use pdf::file::File;
+
 
 
 fn main() -> Result<(),PdfError> {
 
-    let mut file = File::open("a.pdf").unwrap();
+    let font_family = genpdf::fonts::from_files("./fonts","ShantellSans-VariableFont_BNCE,INFM,SPAC,wght",None)
+        .expect("Could not load font family");
 
-    
+    let mut doc = genpdf::Document::new(font_family);
+
+    doc.set_title("Telemetry Report");
+    let mut decorator = genpdf::SimplePageDecorator::new();
+
+    doc.set_page_decorator(decorator);
+
+    doc.push(genpdf::elements::Paragraph::new("Telemetry reporting from Optus."));
+
+    //let mut writer = BufWriter::new(inner)
+    let mut buff : Vec<u8> = vec![];
+
+    let w = BufWriter::new(buff);
+
+    let pdf = doc.render(w);
 
     Ok(())
 }
